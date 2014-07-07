8dbc7a80fd8ff1d099dad095e8fded214604e7f6
Gerard Klijs <gerard@finalist.nl>
2014-07-07 15:49:00 +0200

    add left/right field selection when needed (stolen from selection plugin)

--- dashboard-plugins/addfield-plugin/src/main/java/com/finalist/plugin/AddFieldPluginResource.java
+++ dashboard-plugins/addfield-plugin/src/main/java/com/finalist/plugin/AddFieldPluginResource.java
@@ -1,18 +1,26 @@
 package com.finalist.plugin;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.wicket.util.string.Strings;
 import org.onehippo.cms7.essentials.dashboard.ctx.PluginContext;
 import org.onehippo.cms7.essentials.dashboard.rest.BaseResource;
 import org.onehippo.cms7.essentials.dashboard.rest.ErrorMessageRestful;
 import org.onehippo.cms7.essentials.dashboard.rest.MessageRestful;
 import org.onehippo.cms7.essentials.dashboard.rest.PostPayloadRestful;
+import org.onehippo.cms7.essentials.dashboard.utils.GlobalUtils;
+import org.onehippo.cms7.essentials.dashboard.utils.TemplateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.jcr.ImportUUIDBehavior;
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
 import javax.servlet.ServletContext;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 
@@ -34,6 +42,24 @@ public class AddFieldPluginResource extends BaseResource {
                 return new MessageRestful("Field to be added: " + fieldName + " to documentType "+ documentType);
             }
         return new ErrorMessageRestful("Failed to receive field");
+    }
+
+    /**
+     * Import an XML resource into the repository, after mustache-processing.
+     *
+     * @param resourcePath   path to obtain resource.
+     * @param placeholderMap map with placeholder values
+     * @param destination    target parent JCR node for the import
+     * @throws java.io.IOException
+     * @throws javax.jcr.RepositoryException
+     */
+    private void importXml(final String resourcePath, final Map<String, String> placeholderMap, final Node destination)
+            throws IOException, RepositoryException
+    {
+        final InputStream stream = getClass().getResourceAsStream(resourcePath);
+        final String processedXml = TemplateUtils.replaceStringPlaceholders(GlobalUtils.readStreamAsText(stream), placeholderMap);
+        destination.getSession().importXML(destination.getPath(), IOUtils.toInputStream(processedXml),
+                ImportUUIDBehavior.IMPORT_UUID_COLLISION_REPLACE_EXISTING);
     }
 
 }
--- dashboard-plugins/addfield-plugin/src/main/resources/META-INF/resources/plugins/addFieldPlugin/addFieldPlugin.js
+++ dashboard-plugins/addfield-plugin/src/main/resources/META-INF/resources/plugins/addFieldPlugin/addFieldPlugin.js
@@ -4,25 +4,38 @@
             .controller('addfieldpluginCtrl', function ($scope, $sce, $log, $rootScope, $http) {
                 var endpointDocuments = $rootScope.REST.documents;
                 var endpointAddField = $scope.endpoint = $rootScope.REST.dynamic + 'add-field/';
+                $scope.pluginId = "addFieldPlugin";
                 $scope.fieldName = null;
                 $scope.selectedDocumentType = null;
 
 
                 $scope.addField = function () {
+                    var payload = Essentials.addPayloadData("fieldName", $scope.fieldName, null);
+                    Essentials.addPayloadData("selectedDocumentType", $scope.selectedDocumentType.name, payload);
+                    Essentials.addPayloadData("fieldPosition", $scope.fieldPosition, payload);
+                    $http.post(endpointAddField, payload).success(function (data) {
+                    });
+                };
+                $scope.positionMap = {
+                    '${cluster.id}.right.item': 'right',
+                    '${cluster.id}.left.item' : 'left'
+                };
 
-                var payload = Essentials.addPayloadData("fieldName", $scope.fieldName, null);
-                Essentials.addPayloadData("selectedDocumentType", $scope.selectedDocumentType.name, payload);
-                $http.post(endpointAddField, payload).success(function (data) {
+                // when changing the document type, set the default position and retrieve a fresh list of fields
+                $scope.$watch('selectedDocumentType', function (newDocType) {
+                    if (newDocType) {
+                        $scope.positionMatters = newDocType.fieldLocations.length > 1;
+                        $scope.fieldPosition = newDocType.fieldLocations[0];
+                        reloadSelectionFields(newDocType);
+                    }
+                }, true);
 
+                //############################################
+                // INITIALIZE APP:
+                //############################################
+                $http.get(endpointDocuments).success(function (data) {
+                    $scope.documentTypes = data;
                 });
-            };
-
-            //############################################
-            // INITIALIZE APP:
-            //############################################
-            $http.get(endpointDocuments).success(function (data) {
-                $scope.documentTypes = data;
-            });
 
             })
 })();
